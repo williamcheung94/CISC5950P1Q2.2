0b1910f001f8fcf338e8ee495e8aa18a298fef53
williamcheung94 <61169234+williamcheung94@users.noreply.github.com>
2020-03-27 17:03:25 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ name=$1
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-test/project1/NBA/data/NBA1_6/input
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-test/project1/NBA/data/NBA1_6/output
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1/NBA/data/NBA1_6/input
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal -p /mapreduce-test/project1/NBA/data/shot_logs.csv /mapreduce-test/project1/NBA/data/NBA1_6/input
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal -p shot_logs.csv /mapreduce-test/project1/NBA/data/NBA1_6/input
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file /mapreduce-test/project1/NBA/data/mapper1a.py -mapper "/mapreduce-test/project1/NBA/data/mapper1a.py $name" \
 -file /mapreduce-test/project1/NBA/data/reducer1a.py -reducer /mapreduce-test/project1/NBA/data/reducer1a.py \
